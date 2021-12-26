4dd2db161306a36034b7549199d80a174147cbd8
Edozie <phynnedo@gmail.com>
2021-12-26 23:27:06 +0100

    Added Oncancel function to the form

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
-import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState(2020)
@@ -10,19 +10,17 @@ const Expenses = (props) => {
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear)
     }
+
+    const filteredExpenses = props.items.filter((expense)  => {
+        return expense.date.getFullYear().toString() === filteredYear
+    })
+    
     
     return (
         <div>
             <Card className = "expenses">
                 <ExpensesFilter onChangeFilter = {filterChangeHandler} selected = {filteredYear}/>
-                {props.items.map((expense) => (
-                    <ExpenseItem
-                        key = {expense.id}
-                        title = {expense.title}
-                        amount = {expense.amount}
-                        date = {expense.date}
-                        />))}
-                
+                <ExpensesList items = {filteredExpenses}/>
             </Card>
         </div>
         
